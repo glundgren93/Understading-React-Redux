8e19f26d950b3ee0ad240dc9233f74bd6535e200
glundgren93 <glundgren93@gmail.com>
2016-04-16 02:41:15 -0300

    create video list item and improved video list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // props = properties passed to VideoList
 // in functional components, props are arguments
 const VideoList = (props) => {
+
+  // Create new array containing each video inside a VideoListItem
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
